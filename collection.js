e590f75035b2ec2501790e2c744a2918ed9c33e5
RobertHJones <roberthuwjones@outlook.com>
2021-12-06 17:55:41 +0000

    taskscomplete

---
+++ collection.js
@@ -0,0 +1,21 @@
+let myCollection = [
+    {
+      name: "School of Code mug",
+      count: 1,
+      whatILike: "It has my cute pixel character on it!"
+    },
+    {
+      name: "School of Code hat",
+      count: 2,
+      whatILike: "An often overlooked fashion accessory"
+    },
+    {
+      name: "School of Code pillow",
+      count: 1,
+      whatILike: "Eat. Sleep. Code. Repeat :)"
+    }
+  ];
+  console.log(myCollection);
+
+// Use Node's default exporting syntax in collection.js to export the myCollection variable so that it's ready to import elsewhere in your folder
+  export default myCollection;
--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
-  console.log(myCollection);
+// import myCollection at the top of index.js
+import myCollection from "./collection.js";
+
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
 
   function describeItem(item) {
     if (item.count === 1) {
-      console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`)
+      console.log(`I have a ${chalk.cyan.underline(item.name)}. Here's what I like about it: ${chalk.green.italic(item.whatILike)}`)
     } else {
-      console.log(`I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`)
+      console.log(`I have ${chalk.yellow(item.count)} ${chalk.cyan(item.name)}s. Here's what I like about them: ${chalk.green.bgWhite(item.whatILike)}`)
     }
   }
 describeItem(myCollection[1]);
+
+function describeCollection(arr) {
+  arr.forEach(describeItem);
+  
+};
+
+describeCollection(myCollection);
